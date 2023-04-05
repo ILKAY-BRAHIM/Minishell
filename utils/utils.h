93a2cb4fbd0c7989140fbfb2f25d937a7bb11d6c
Brahim Chifour <bchifour@c1r7p1.1337.ma>
2023-04-05 15:09:37 +0000

    push 1

--- utils/utils.h
+++ utils/utils.h
@@ -6,11 +6,7 @@
 /*   By: bchifour <bchifour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 06:14:07 by rrasezin          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/04/05 12:40:34 by bchifour         ###   ########.fr       */
-=======
-/*   Updated: 2023/04/02 00:58:54 by rrasezin         ###   ########.fr       */
->>>>>>> adcbe6a56695efca909c06bedf216636714f7f07
+/*   Updated: 2023/04/05 15:00:44 by bchifour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +26,6 @@ void	ft_bzero(void *src, size_t len);
 int		ft_strcmp(char *s1, char *s2);
 void	ft_putstr(char *str);
 char	*ft_strdup(char *s1);
-<<<<<<< HEAD
 char	*ft_strjoin(char *s1, char *s2);
-=======
-char	*ft_strchr(const char *s, int c);
-t_env	*new_list(char *org_env);
-void	add_back(t_env **list, t_env *new);
-void	add_front(t_env **list, t_env *new);
->>>>>>> adcbe6a56695efca909c06bedf216636714f7f07
 
 #endif
