48b8b965afdf62833827361e3d4132766c331e23
Rajae Rasezine <rrasezin@c1r7p3.1337.ma>
2023-04-07 18:42:40 +0000

    .

--- utils/ft_strchr.c
+++ utils/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rrasezin <rrasezin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 15:12:12 by rrasezin          #+#    #+#             */
-/*   Updated: 2023/03/30 23:04:06 by rrasezin         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:42:24 by rrasezin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,4 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)s);
 	return (NULL);
 }
+
